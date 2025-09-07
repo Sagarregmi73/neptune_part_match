ed4c424119aa3bd12941dff20dfefb587e7ea681
sagar regmi <sagarregmi026@gmail.com>
2025-09-07 15:59:04 +0545

    updated s3_client.py

--- lib/core/aws/s3_client.py
+++ lib/core/aws/s3_client.py
@@ -5,19 +5,7 @@ from botocore.exceptions import ClientError
 from lib.core.logging import logger
 from dotenv import load_dotenv
 
-# Try multiple possible env paths
-ENV_PATHS = [
-    "env/local.env",                        # local dev
-    "/home/ec2-user/neptune_part_match/.env"  # EC2
-]
-
-for path in ENV_PATHS:
-    if os.path.exists(path):
-        load_dotenv(dotenv_path=path)
-        logger.info(f"Loaded environment from {path}")
-        break
-else:
-    logger.warning("No .env file found, relying on system environment variables")
+load_dotenv(dotenv_path="env/local.env")
 
 AWS_REGION = os.getenv("AWS_REGION")
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
@@ -46,3 +34,4 @@ def download_file(key: str, dest_path: str):
     except ClientError as e:
         logger.error(f"Failed to download {key} from S3: {e}")
         raise
+
