c053dbde92c651e7a1400dc1c71114766ceb1960
sagar regmi <sagarregmi026@gmail.com>
2025-09-14 11:24:04 +0545

    update whole code

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from dotenv import load_dotenv
 load_dotenv("local.env")  # must be first
-
+import os
 from fastapi import FastAPI
 from lib.app.adapter.input.api.v1.routers import api_router
 
 app = FastAPI(title="PartNumber Matching API")
 app.include_router(api_router)
+
+# Quick debug print to verify environment variables
+print("Loaded environment variables:")
+print("NEPTUNE_ENDPOINT =", os.getenv("NEPTUNE_ENDPOINT"))
+print("NEPTUNE_IAM_ROLE_ARN =", os.getenv("NEPTUNE_IAM_ROLE_ARN"))
+print("AWS_REGION =", os.getenv("AWS_REGION"))
