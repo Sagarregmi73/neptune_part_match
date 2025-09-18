11e98924ca83e742065ce6957f8b966817be833c
sagar regmi <sagarregmi026@gmail.com>
2025-09-18 08:23:28 +0545

    update

--- lib/core/utils/container.py
+++ lib/core/utils/container.py
@@ -1,41 +1,37 @@
-# lib/core/utils/container.py
-
 from lib.app.application.use_cases.crud_part_usecase import CrudPartUseCase
 from lib.app.application.use_cases.match_part_usecase import MatchPartUseCase
 from lib.app.application.use_cases.upload_file_usecase import UploadFileUseCase
-
-# Import repositories
 from lib.app.adapter.output.persistence.neptune.neptune_repository import NeptuneRepository
-# In future, you can add:
-# from lib.app.adapter.output.persistence.other_db.other_repository import OtherRepository
 
-# ------------------------
-# Repository Factory
-# ------------------------
-def get_repository(db_type: str = "neptune"):
+# ---------------- Repository Instances ----------------
+# For now, using NeptuneRepository
+neptune_repository = NeptuneRepository()
+
+# ---------------- CRUD Usecase Factory ----------------
+def get_part_repository():
     """
-    Returns the repository instance based on db_type.
-    Defaults to Neptune.
+    Returns the repository instance for parts.
+    Can be extended later for multiple DB support.
     """
-    if db_type == "neptune":
-        return NeptuneRepository()
-    # Example for future DBs:
-    # elif db_type == "other":
-    #     return OtherRepository()
-    else:
-        raise ValueError(f"Unknown repository type: {db_type}")
+    return neptune_repository
 
-# ------------------------
-# Use Cases
-# ------------------------
-def get_part_usecase(db_type: str = "neptune"):
-    repo = get_repository(db_type)
-    return CrudPartUseCase(repo)
+def get_part_usecase():
+    """
+    Returns the Part CRUD usecase instance
+    """
+    return CrudPartUseCase(get_part_repository())
 
-def get_match_usecase(db_type: str = "neptune"):
-    repo = get_repository(db_type)
-    return MatchPartUseCase(repo)
+# ---------------- Match Usecase Factory ----------------
+def get_match_usecase():
+    """
+    Returns the Match usecase instance
+    """
+    return MatchPartUseCase(get_part_repository())
 
-def get_file_usecase(backup_to_s3: bool = False, db_type: str = "neptune"):
-    repo = get_repository(db_type)
-    return UploadFileUseCase(repository=repo, backup_to_s3=backup_to_s3)
+# ---------------- File Upload Usecase Factory ----------------
+def get_file_usecase(backup_to_s3: bool = False):
+    """
+    Returns the File Upload usecase instance.
+    Accepts optional backup_to_s3 flag.
+    """
+    return UploadFileUseCase(repository=get_part_repository(), backup_to_s3=backup_to_s3)
